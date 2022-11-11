263ee1fbffee3a43dbc7663c44bfff379f063d39
developer <monarchkoli12@gmail.com>
2022-11-11 18:35:39 +0530

    First commit

---
+++ removingeanda.rb
@@ -0,0 +1,7 @@
+# Q2. Remove 'a'  and 'e' from string :- 
+# "Hii my name is Suresh" to  "Hii my nm is Sursh
+
+sentence = "Hii my name is Suresh"
+
+name = sentence.delete!("ae")
+puts sentence
---
+++ reverse.rb
@@ -0,0 +1,5 @@
+# Q1. Reverse this string "Ramesh"
+
+name = "Ramesh"
+
+puts name.reverse
